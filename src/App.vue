72aaadcca477eb8e826b3c99399a559377ba4e7a
shruti Biradar <shruti.biradar@accelotics.com>
2024-03-03 12:02:34 +0530

    todoApp

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Todo List</h1>
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="newTodo.title" placeholder="Title">
+      <textarea v-model="newTodo.description" placeholder="Description"></textarea>
+      <button type="submit">Add Todo</button>
+    </form>
+    <div class="todo-list">
+      <TodoItem v-for="(todo, index) in todos" :key="index" :title="todo.title" :description="todo.description"
+        @delete="deleteTodo(index)" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    TodoItem
+  },
+  data() {
+    return {
+      todos: [],
+      newTodo: {
+        title: '',
+        description: ''
+      }
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({ ...this.newTodo });
+      this.newTodo.title = '';
+      this.newTodo.description = '';
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+/* Add your CSS styles here */
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="todo-card">
+    <h2>{{ title }}</h2>
+    <p>{{ description }}</p>
+    <button @click="$emit('delete')">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    description: String
+  }
+}
+</script>
+
+<style scoped>
+.todo-card {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+.todo-card button {
+  margin-top: 10px;
+}
+</style>
